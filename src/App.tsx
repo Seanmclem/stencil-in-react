a3a50f3b7ed94e2006a9e00931e362b6f509f5e4
seanmclem <seanmclements@gmail.com>
2020-08-10 21:51:40 -0400

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -9,19 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <my-component first='sean' middle='m' last='clements'></my-component>
-
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <my-component first='sean' middle='m' last='clem-react'></my-component>
       </header>
     </div>
   );
